72ce09f7e423226e0d6d7973084bb0ffdec9e320
Bohdan Chornokondratenko <chornoko@goldbeck.biz>
2025-03-04 17:01:48 +0100

    CRUD WiP

--- server-asp/Program.cs
+++ server-asp/Program.cs
@@ -5,7 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services
 .AddGraphQLServer()
-.AddQueryType<Query>();
+.AddQueryType<Query>()
+.AddMutationType<Mutation>();
+
+builder.Services.AddSingleton<TodoRepository, TodoRepository>();
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddCors(options =>
@@ -39,17 +42,47 @@ app.MapGraphQL();
 
 app.Run();
 
-public class Query
+public class TodoRepository
 {
-  public Todo[] Todos() =>
-    [
+  public readonly List<Todo> todos = [
     new Todo
     {
       Id = "0",
          Done= true,
          Text= "123"
     }
-    ];
+  ];
+}
+
+public class Query([Service] TodoRepository todoRepository)
+{
+  public Todo[] Todos() => [.. todoRepository.todos];
+}
+
+public class Mutation([Service] TodoRepository todoRepository)
+{
+
+  public bool CreateTodo(Todo todo)
+  {
+    todoRepository.todos.Add(todo);
+    return true;
+  }
+
+  public bool DeleteTodo(string id)
+  {
+    var idx = todoRepository.todos.FindIndex(todo => todo.Id == id);
+    todoRepository.todos.RemoveAt(idx);
+    return true;
+  }
+
+  // BUG: changing "done" in frontend doesn't work (doing so though the gql web view works)
+  public bool UpdateTodo(Todo todo)
+  {
+    var idx = todoRepository.todos.FindIndex(todo => todo.Id == todo.Id);
+    todoRepository.todos[idx] = todo;
+    return true;
+  }
+
 }
 
 public record Todo
